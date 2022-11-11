d40050b5bd8877d6243d9067688a4416a9295d37
Michael Vessia <michael@vessia.net>
2022-11-10 23:04:35 -0500

    Simplify whichkey config

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -220,6 +220,7 @@ vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc
 vim.keymap.set('n', '<leader>fw', require('telescope.builtin').grep_string, { desc = '[F]ind current [W]ord' })
 vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, { desc = '[F]ind by [G]rep' })
 vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { desc = '[F]ind [D]iagnostics' })
+vim.keymap.set('n', '<leader>fn', ':Telescope notify<cr>', { desc = '[Find] [N]otifications' })
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
@@ -430,11 +431,7 @@ vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 vim.keymap.set('n', '<leader>e', ':Neotree reveal toggle<cr>', { desc = 'Toggle Tr[E]e' })
 
 -- Enable Whichkey
-local wk = require("which-key")
-local mappings = {}
-local opts = { prefix = '<leader>' }
-wk.register(mappings, opts)
-wk.setup()
+require("which-key").setup()
 
 -- Enable vim+tmux nav
 vim.g.tmux_navigator_no_mappings = 1
