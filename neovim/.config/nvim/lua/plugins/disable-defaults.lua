e74a94e8bbfa1157a6ac8a69821af8f707ecd47e
Michael Vessia <michael@vessia.net>
2023-05-06 01:42:25 -0400

    Remove gitsigns in favor of a simpler plugin

--- neovim/.config/nvim/lua/plugins/disable-defaults.lua
+++ neovim/.config/nvim/lua/plugins/disable-defaults.lua
@@ -2,4 +2,6 @@ return {
   -- disable leap and flit which both override default vim binds
   { "ggandor/leap.nvim", enabled = false },
   { "ggandor/flit.nvim", enabled = false },
+  -- disable gitsigns since it has so many conflicts with fugitive
+  { "lewis6991/gitsigns.nvim", enabled = false },
 }
