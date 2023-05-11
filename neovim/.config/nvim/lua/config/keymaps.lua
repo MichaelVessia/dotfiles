2bcf7eff6407b7dcb4b9a91ea01eeab5888bed33
Michael Vessia <michael.vessia@flosports.tv>
2023-05-11 10:18:47 -0400

    Updating binds and trying to fix vim tmux navigation

--- neovim/.config/nvim/lua/config/keymaps.lua
+++ neovim/.config/nvim/lua/config/keymaps.lua
@@ -2,23 +2,20 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local Util = require("lazyvim.util")
-
-local function map(mode, lhs, rhs, opts)
-  local keys = require("lazy.core.handler").handlers.keys
-  ---@cast keys LazyKeysHandler
-  -- do not create the keymap if a lazy keys handler exists
-  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
-    opts = opts or {}
-    opts.silent = opts.silent ~= false
-    vim.keymap.set(mode, lhs, rhs, opts)
-  end
-end
-
 -- Unbind the extremely annoying binds that come with lazyvim by default
-map("n", "<A-j>", "<Nop>", { desc = "Move down" })
-map("n", "<A-k>", "<Nop>", { desc = "Move up" })
-map("i", "<A-j>", "<Nop>", { desc = "Move down" })
-map("i", "<A-k>", "<Nop>", { desc = "Move up" })
-map("v", "<A-j>", "<Nop>", { desc = "Move down" })
-map("v", "<A-k>", "<Nop>", { desc = "Move up" })
+vim.keymap.set("n", "<A-j>", "<Nop>")
+vim.keymap.set("n", "<A-k>", "<Nop>")
+vim.keymap.set("i", "<A-j>", "<Nop>")
+vim.keymap.set("i", "<A-k>", "<Nop>")
+vim.keymap.set("v", "<A-j>", "<Nop>")
+vim.keymap.set("v", "<A-k>", "<Nop>")
+
+-- Command aliases for typos
+vim.keymap.set("c", "W", "w")
+vim.keymap.set("c", "Q", "q")
+vim.keymap.set("c", "WQ", "wq")
+vim.keymap.set("c", "Wq", "wq")
+
+-- Move highlighted things around
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
