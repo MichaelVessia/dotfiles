27d72f5073aa664ff9feb3f1f2fdc2429046dd85
Michael Vessia <michael.vessia@flosports.tv>
2024-08-21 09:49:46 -0400

    replace harpoon2 with arrow

--- neovim/.config/nvim/lua/plugins/vim-fugitive.lua
+++ neovim/.config/nvim/lua/plugins/vim-fugitive.lua
@@ -5,11 +5,11 @@ return {
     keys = {
       -- Maybe I can muscle memory myself into using :G
       --{ "<leader>gs", ":G<cr>", { desc = "[G]it [S]tatus" } },
-      { "<leader>gl", ":Git blame<cr>", { desc = "[G]it B[l]ame" } },
+      --{ "<leader>gl", ":Git blame<cr>", { desc = "[G]it B[l]ame" } },
       { "<leader>gr", ":GBrowse<cr>", { desc = "[G]it [R]epo" } },
       { "<leader>gp", ":Git push -u origin HEAD<cr>", { desc = "[G]it [P]ush" } },
       { "<leader>gpn", ":Git push -u origin HEAD --no-verify<cr>", { desc = "[G]it [P]ush [N]o verify" } },
-      { "<leader>gf", ":Git pull<cr>", { desc = "[G]it [F]etch (Pull)" } },
+      --{ "<leader>gf", ":Git pull<cr>", { desc = "[G]it [F]etch (Pull)" } },
       { "<leader>gd", ":Git diff<cr>", { desc = "[G]it [D]iff" } },
     },
   },
